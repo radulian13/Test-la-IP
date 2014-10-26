3d7b8c75197fafcbbe7720a158f823607fa3cfe1
Radulian13 <raduliandefta@yahoo.com>
2014-10-26 15:24:21 +0200

    Acesta este Lab2 IP

---
+++ Lab2/src/com/example/babies/babies.java
@@ -0,0 +1,71 @@
+package com.example.babies;
+
+/**
+ * Created by VoldHouse on 10/26/2014.
+ */
+public class babies {
+    //clasa separata care va fi importata in main
+    //au fost generati constructor, getteri si setteri
+    //click dreapta generate->constructor generate->getter and setter
+    private String nume;
+    private boolean isMale;
+    private double weight;
+    private double decibels;
+    private int numPoops=0;
+
+    public babies(String nume, boolean isMale, double weight, double decibels, int numPoops) {
+        this.nume = nume;
+        this.isMale = isMale;
+        this.weight = weight;
+        this.decibels = decibels;
+        this.numPoops = numPoops;
+    }
+
+    public String getNume() {
+        return nume;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public boolean isMale() {
+        return isMale;
+    }
+
+    public void setMale(boolean isMale) {
+        this.isMale = isMale;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public double getDecibels() {
+        return decibels;
+    }
+
+    public void setDecibels(double decibels) {
+        this.decibels = decibels;
+    }
+
+    public int getNumPoops() {
+        return numPoops;
+    }
+
+    public void setNumPoops(int numPoops) {
+        this.numPoops = numPoops;
+    }
+    public void afisare()
+    {
+        System.out.println("Detaliile bebelusului sunt:\n" +
+                "Nume:"+getNume() +
+                "\nDecibeli:"+getDecibels()+
+                "\nNo.Poops:"+getNumPoops()+
+                "\nGreutate:"+getWeight());
+    }
+}
---
+++ Lab2/src/com/example/test/Test.java
@@ -0,0 +1,39 @@
+package com.example.test;
+import com.example.babies.babies;
+
+/**
+ * Created by VoldHouse on 10/26/2014.
+ */
+public class Test {
+    public static void main(String[] args){
+        //aceasta este functia main care va fi apelata
+        //si va rula toate functiile
+
+        System.out.println("Bine ati venit la Lab2!");
+
+        String[] ArrayOfStrings={"Subpunctul1","Subpunctul2","Subpunctul3"};
+        for(String string:ArrayOfStrings){
+            System.out.println("Vom realiza in acest lab: "+ string );
+        }
+        System.out.println(ArrayOfStrings[0]);
+        //am creat o clasa separata babies, am importat-o si am creat un obiect de tip babies
+        babies b=new babies("Andrei",true,20,90,2);
+        b.afisare();//functie de afisare in clasa babies
+        //setez numele bebelusului alexandru
+        b.setNume("Alexandru");
+        b.afisare();
+        System.out.println(ArrayOfStrings[1]);
+        for(int i=0;i<=ArrayOfStrings.length;i++){
+            try{
+                System.out.println("This is a string: "+ ArrayOfStrings[i] );
+            }
+            catch(Exception e)
+            {
+                System.out.println("Exceptia este: "+e);
+            }
+
+        }
+        System.out.println(ArrayOfStrings[2]);
+        System.out.println("Am creat fisierul .gitignore!");
+    }
+}
